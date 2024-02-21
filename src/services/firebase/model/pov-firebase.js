2a97f43cda7c124d99a4b53343c24e82b6d6924d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-21 17:52:34 +0300

    pov firestore

--- src/services/firebase/model/pov-firebase.js
+++ src/services/firebase/model/pov-firebase.js
@@ -1,19 +1,71 @@
-import { loadDocsData, saveDocData } from "../firebase-firestore";
+import {
+  deleteDocData,
+  loadDocDataById,
+  loadDocsData,
+  saveDocData,
+  setDocData,
+} from "../firebase-firestore";
 
 const docName = "povs";
 
 export const savePoVFirebase = async (pov = {}) => {
   try {
-    return await saveDocData(docName, pov);
+    const { title, subtitle, points, owner } = pov;
+    const povData = {
+      title: title,
+      subtitle: subtitle,
+      points: points,
+      owner: owner,
+    };
+    return await saveDocData(docName, "", povData);
   } catch (error) {
     throw error;
   }
 };
 
-export const getPoVsFirebase = async()=>{
+export const getPoVsFirebase = async () => {
   try {
-    return await loadDocsData(docName)
+    return await loadDocsData(docName);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
+
+export const getPoVFirebase = async (povId) => {
+  try {
+    return await loadDocDataById(docName, povId);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// export const getPoVByOwnerFirebase = async (userId, povId) => {
+//   try {
+//     return await loadDocDataById(docName, userId, povId); // todo: match owner's pov
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+export const updatePoVFirebase = async (povId, pov) => {
+  try {
+    const { title, subtitle, points, owner } = pov;
+    const povData = {
+      title: title,
+      subtitle: subtitle,
+      points: points,
+      owner: owner,
+    };
+    return await setDocData(docName, povId, "", povData);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deletePoVFirebase = async (povId) => {
+  try {
+    return await deleteDocData(docName, povId);
+  } catch (error) {
+    throw error;
+  }
+};
